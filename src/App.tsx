8ac969663d87742c110c31418a753f2de5cef259
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2025-09-13 14:40:23 +0530

    Add page route page
    Co-Authored-By: Dahamneth W.M.S.S <saninduofficial123@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "../src/components/Navbar" ;
-import Home from "./pages/Home";
-import Products from "./pages/Products";
-import Blogs from "./pages/Blogs";
-import Projects from "./pages/Projects";
-import About from "./pages/About";
+import PageRoute from "./Context/PageRoute";
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
-    </Router>
-  );
+  return <PageRoute />;
 }
 
 export default App;
---
+++ src/Context/PageRoute.tsx
@@ -0,0 +1,25 @@
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "../components/Navbar" ;
+import Home from "../pages/Home";
+import Products from "../pages/Products";
+import Blogs from "../pages/Blogs";
+import Projects from "../pages/Projects";
+import About from "../pages/About";
+
+function PageRoute() {
+  return (
+       <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default PageRoute
