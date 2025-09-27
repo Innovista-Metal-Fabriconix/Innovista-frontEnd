2eaaf4d077778449a5d1bddf9867bbf891e289e6
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2025-09-27 19:05:21 +0530

    Add product page

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Products() {
   return (
-    <div>Products</div>
-  )
+    <>
+      <div>Products</div>
+      <Link to="/product-details">Go to Product Details</Link>
+    </>
+  );
 }
 
-export default Products
+export default Products;
