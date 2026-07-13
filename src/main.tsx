f5de9cfaf6b506ece428dc00402415a0fa20b0ef
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2026-07-13 08:52:39 +0530

    Add google tag manager

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import TagManager from "react-gtm-module";
+
+TagManager.initialize({
+  gtmId: "GTM-5JXDQGGT",
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
